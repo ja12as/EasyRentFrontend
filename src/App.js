8ab37271284a86c77363d71894a33b7745801a2d
Jose Armando Avendaño Saucedo <126033004+ja12as@users.noreply.github.com>
2024-02-07 08:47:40 -0500

    Jose_Armando_LT_Creacion_Del_Encarpetado

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nanbar from './component/Nanbar'
+import Footer from './component/Footer'
+
+const App = () => {
+  return (
+    <>
+    <Nanbar/>
+    <Footer/>
+  </>
+  )
+}
+
+export default App
