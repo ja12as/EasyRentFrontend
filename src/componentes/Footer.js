39b058952f39c851f20ecfa41eac6ab374362adf
Cristian Escobar Berrio <caberrio86@misena.edu.co>
2024-02-10 18:30:39 -0500

    CristianBerrio_Creacion_Main

---
+++ src/componentes/Footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+import '../styles/Footer.css'
+
+
+export const Footer = () => {
+    return (
+   <>
+   <h1 className="inicio">Descubre la magia en cada lugar...</h1>
+      
+   </>
+
+    )
+}
---
+++ src/componentes/Logocontainer.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import '../styles/Logocontainer.css'
+
+export const LogoContainer = () => {
+  return (
+    <div className="logo-container">
+      <div className="logo">
+      <img src="logo1.jpg" alt="logo " className='logo' />
+      <p >Cabinas</p>
+      </div>
+      <div className="logo">
+      <img src="logo1.jpg" alt="Logo 3" className='logo'  />
+      <p >Cabinas</p>
+      </div>
+      <div className="logo">
+      <img src="logo1.jpg" alt="Logo 3" className='logo'  />
+      <p  >Cabinas</p>
+      </div>
+      <div className="logo">
+      <img src="logo1.jpg" alt="Logo 3" className='logo'  />
+      <p >Cabinas</p>
+      </div>
+      <div className="logo">
+      <img src="logo1.jpg" alt="Logo 3" className='logo'  />
+      <p >Cabinas</p>
+      </div> 
+      <div className="logo">
+      <img src="logo1.jpg" alt="Logo 3" className='logo'  />
+      <p >Cabinas</p>
+      </div> 
+      <div className="logo">
+      <img src="logo1.jpg" alt="Logo 3" className='logo'  />
+      <p >Cabinas</p>
+      </div> 
+      <div className="logo">
+      <img src="logo1.jpg" alt="Logo 3" className='logo'  />
+      <p >Cabinas</p>
+      </div> 
+      
+      
+      
+      {/* Agrega más imágenes según sea necesario */}
+    </div>
+  );
+};
+
+export default LogoContainer;
